9f6b0242b4c28d0b8b73749b694d6803642316d6
chukmunnlee <chukmunnlee@gmail.com>
2022-11-18 13:55:17 +0800

    updated ingress grpc

--- k8s/gencerts.sh
+++ k8s/gencerts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 IP_ADDR="$(minikube ip)"
-openssl genrsa -out server.key 4096
-openssl req -x509 -key server.key -out server.crt \
-	-subj '/CN=bgg-grpc' \
-	-addext "subjectAltName = DNS:bgg-grpc-${IP_ADDR}.nip.io, DNS:localhost, IP:127.0.0.1, IP:${IP_ADDR}" \
-	-addext 'keyUsage = keyEncipherment, digitalSignature' \
-	-addext 'extendedKeyUsage = serverAuth' \
-	-sha256 -days 365 
 
-kubectl create secret tls bgg-grpc-tls --key=server.key --cert=server.crt -oyaml --dry-run=client | tee bgg-grpc-tls.yaml
+openssl genrsa -out server.key 4096
+
+openssl req -new -key server.key -out server.csr \
+	-subj "/CN=system:node:bgg-grpc/O=system:nodes" \
+	-addext "subjectAltName = DNS:bgg-grpc-${IP_ADDR}.nip.io, DNS:localhost, IP:127.0.0.1,IP:${IP_ADDR}" \
+	-addext 'keyUsage = keyEncipherment, digitalSignature' \
+	-addext 'extendedKeyUsage = serverAuth' 
+
